983cf182fd4e63f6f37e3deba41972444ca26182
SrijanSuresh <126934224+SrijanSuresh@users.noreply.github.com>
2024-04-08 18:04:11 -0500

    Added body parser and POST to capture user input username/password, (only for testing purpose for authentication)

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express")
+const bodyParser = require('body-parser');
+
 const app = express()
+
+// Middleware to parse URL-encoded bodies (as sent by HTML forms)
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.set('view engine', 'ejs')
 
@@ -7,9 +13,22 @@ app.get("/", (req, res) => {
     console.log("Here")
     res.render('index', {text23232:"World"})
 });
+
 app.get("/login", (req,res) =>{
     res.render('login')
 });
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password; // assuming you have a password field
+
+    // You can use the username and password variables here as needed
+    res.render('index', {
+        username: username,
+        password: password // pass the password to the template if needed
+    });
+});
+
 
 
 const userRouter = require('./routes/users')
