d241668e64e806d7def498cfc0f5ccabd8aa57d9
SrijanSuresh <126934224+SrijanSuresh@users.noreply.github.com>
2024-04-08 19:20:40 -0500

    customized css for login page, added more componenets to tailwind config, deleted duplicate login.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'bungee': ['Bungee Spice', 'cursive'],
+        // Add more custom font families if needed
+      },
+    },
   },
-  plugins: [],
-}
 
+  plugins: [
+    require('@tailwindcss/forms'),
+    plugin(function({ addUtilities }) {
+      const newUtilities = {
+        '.font-bungee': {
+          fontFamily: 'Bungee Spice, cursive',
+        },
+        // Define other custom utilities if needed
+      };
+
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    }),
+  ],
+}
