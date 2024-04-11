154df8805457b7ff86dbd24f7d24e9b7cd06a720
SrijanSuresh <126934224+SrijanSuresh@users.noreply.github.com>
2024-04-11 13:26:48 -0500

    attempted to add google maps js api map

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-const { pool } = require ('pg')
-const pool = new Pool({
-    host: 'db',
-    port: '5432',
-    user: 'currentUser',
-    password: '12345',
-    database: 'db123'
-})
+//const { pool } = require ('pg')
+// const pool = new Pool({
+//     host: 'db',
+//     port: '5432',
+//     user: 'currentUser',
+//     password: '12345',
+//     database: 'db123'
+// })
 
-module.exports = pool
+// module.exports = pool
