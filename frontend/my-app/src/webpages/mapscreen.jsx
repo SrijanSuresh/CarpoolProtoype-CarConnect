34617cfdfc9a18e1ffb169de96f89d86f883e770
SrijanSuresh <126934224+SrijanSuresh@users.noreply.github.com>
2024-06-28 17:17:14 -0500

    configured default mapscreen

---
+++ frontend/my-app/src/webpages/mapscreen.jsx
@@ -0,0 +1,14 @@
+// MapScreen.jsx
+import React from 'react';
+import Map from './map';
+
+const MapScreen = () => {
+    return (
+        <div>
+            <h1>My Map Screen</h1>
+            <Map />
+        </div>
+    );
+}
+
+export default MapScreen;
