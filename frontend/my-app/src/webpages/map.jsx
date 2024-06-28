f3a64ef8a35186efb56acc701ef195e3646b7378
SrijanSuresh <126934224+SrijanSuresh@users.noreply.github.com>
2024-06-28 17:16:57 -0500

    configured map from leaflet

---
+++ frontend/my-app/src/webpages/map.jsx
@@ -0,0 +1,33 @@
+// Map.jsx
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Fix for default marker icon not appearing
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+});
+
+const Map = () => {
+    const position = [51.505, -0.09]; // Example coordinates
+
+    return (
+        <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={position}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+    );
+}
+
+export default Map;
